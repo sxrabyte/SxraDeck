3ab68eb578688c670ba4035117ae73cc4b947336
serena <serena.kneringer@gmail.com>
2026-06-05 21:35:31 +0200

    ~ First Steps: - Started coding sxradeck/ui/app.py and added basic menu - added basic styling to menu - added banner to the top of the menu using a static widget

---
+++ sxradeck/ui/app.py
@@ -0,0 +1,28 @@
+from textual.app import App
+from textual.widgets import Header, ListView, ListItem, Label , Static
+from pathlib import Path
+
+class SxraDeck(App):
+
+    CSS_PATH = str(Path(__file__).parent / "styles.tcss")
+
+    def compose(self):
+
+        base_dir = Path(__file__).parent.parent.parent
+        banner = base_dir / "sxradeck" / "ui" / "banner.txt"
+
+        with open(banner, "r", encoding="utf-8") as f:
+            banner = f.read()
+
+        yield Static(banner)
+        yield Header()
+        yield ListView(
+            ListItem(Label("🐾 recon")),
+            ListItem(Label("📡 airwave")),
+            ListItem(Label("🔵 pawprint")),
+            ListItem(Label("💀 ghostmode")),
+            ListItem(Label("🌸 settings"))
+        )
+if __name__ == "__main__":
+    app = SxraDeck()
+    app.run()
